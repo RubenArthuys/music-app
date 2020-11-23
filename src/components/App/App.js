e49c6e1e67a18a97695dbb942954dadc5a897f10
RubenArthuys <ruben.arthuys@gmail.com>
2020-11-23 20:27:22 +0100

    new

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,9 +5,9 @@ function App() {
     <div className="App">
       <h1>Hello </h1>
       <h2>Work more today </h2>
-      <h3>Come on man</h3>
-      <h4>Push it</h4>
-      <h5>Work it while it's hot</h5>
+      <h3>Come on man </h3>
+      <h4>Push it </h4>
+      <h5>Work it while it's hot </h5>
     </div>
   );
 }
