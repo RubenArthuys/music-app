58162d9a0437295f597ad482d285cfc48dded0d9
RubenArthuys <ruben.arthuys@gmail.com>
2020-11-24 20:48:25 +0100

    new

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,6 +9,8 @@ function App() {
       <h4>Push it </h4>
       <h5>Work it while it's hot </h5>
       <h6>You can do it</h6>
+      <h7>Just a slight dip in motivation</h7>
+      <h8>Don't loose the wheel turning</h8>
     </div>
   );
 }
