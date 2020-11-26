f4748a7566dadf7d768457956540b48034c8d642
RubenArthuys <ruben.arthuys@gmail.com>
2020-11-26 09:27:58 +0100

    new

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,16 +5,6 @@ function App() {
     <div className="App">
       <h1>Hello </h1>
       <h2>Work more today </h2>
-      <h3>Come on man </h3>
-      <h4>Push it </h4>
-      <h5>Work it while it's hot </h5>
-      <h6>You can do it</h6>
-      <h7>Just a slight dip in motivation</h7>
-      <h8>Don't loose the wheel turning</h8>
-      <h9>Oh boy</h9>
-      <h10>This is getting sad</h10>
-      <h11>Get it done ya c*nt</h11>
-      <h12>Face the facts</h12>
     </div>
   );
 }
